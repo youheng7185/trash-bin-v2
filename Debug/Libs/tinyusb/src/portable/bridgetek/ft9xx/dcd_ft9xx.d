10d26ed6317d9434497b053874b371fdbd813f91
youheng7185 <youheng7185@gmail.com>
2025-05-03 08:47:50 +0800

    untrack debug folder

--- Debug/Libs/tinyusb/src/portable/bridgetek/ft9xx/dcd_ft9xx.d
+++
@@ -1,9 +0,0 @@
-Libs/tinyusb/src/portable/bridgetek/ft9xx/dcd_ft9xx.o: \
- ../Libs/tinyusb/src/portable/bridgetek/ft9xx/dcd_ft9xx.c \
- ../Libs/tinyusb/src/tusb_option.h \
- ../Libs/tinyusb/src/common/tusb_compiler.h ../Core/Inc/tusb_config.h \
- ../Libs/tinyusb/src/common/tusb_mcu.h
-../Libs/tinyusb/src/tusb_option.h:
-../Libs/tinyusb/src/common/tusb_compiler.h:
-../Core/Inc/tusb_config.h:
-../Libs/tinyusb/src/common/tusb_mcu.h:
--- Debug/Libs/tinyusb/src/portable/chipidea/ci_fs/dcd_ci_fs.d
+++
@@ -1,9 +0,0 @@
-Libs/tinyusb/src/portable/chipidea/ci_fs/dcd_ci_fs.o: \
- ../Libs/tinyusb/src/portable/chipidea/ci_fs/dcd_ci_fs.c \
- ../Libs/tinyusb/src/tusb_option.h \
- ../Libs/tinyusb/src/common/tusb_compiler.h ../Core/Inc/tusb_config.h \
- ../Libs/tinyusb/src/common/tusb_mcu.h
-../Libs/tinyusb/src/tusb_option.h:
-../Libs/tinyusb/src/common/tusb_compiler.h:
-../Core/Inc/tusb_config.h:
-../Libs/tinyusb/src/common/tusb_mcu.h:
